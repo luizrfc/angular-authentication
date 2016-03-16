dd2d44c7d1a71548e76c2fb39a9194c40adf09e1
mrkrstrndd <maartri@gmail.com>
2016-03-16 16:22:10 +0800

    put some bower

--- angular-jwt/Controllers/ValuesController.cs
+++ angular-jwt/Controllers/ValuesController.cs
@@ -5,15 +5,19 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
+
 namespace angular_jwt.Controllers
 {
-    [Authorize]
+    [AllowAnonymous]
     public class ValuesController : ApiController
     {
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            
+
+                return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
